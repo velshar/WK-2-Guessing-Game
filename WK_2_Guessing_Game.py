9d4c147cab8433a1bb9895d37b4b57c5b41512ca
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-09 21:03:00 -0800

    Week 2 Guessing Game

--- WK_2_Guessing_Game.py
+++ WK_2_Guessing_Game.py
@@ -1,3 +1,35 @@
-import
+import random
 
 def display_title():
+    print("Guess the number!")
+    print()
+    print()
+
+def play_game(LIMIT):
+    number = random.randint(1, LIMIT)
+    print(f"I'm thinking of a number from 1 to {LIMIT}\n")
+    count = 1
+    guess = int(input("Your guess: "))
+
+    while (guess != number):
+        if guess < number:
+            print("Too low.")
+            count += 1
+        elif guess > number:
+            print ("Too high.")
+            count += 1
+        guess = int(input("Your guess: "))
+    print(f" You guessed it in {count} tries.\n")
+
+def main():
+    display_title()
+    again = "y"
+    while again.lower() == "y":
+        LIMIT = int(input("Enter the limit: "))
+        play_game(LIMIT)
+        again = input("Would you like to play again? (y/n): ")
+        print()
+    print("Bye!")
+
+if __name__ == "__main__":
+    main()
